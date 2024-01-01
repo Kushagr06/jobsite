c0b93fbc4a5ac885fc1f0d3cf0f5cb5720ea2e38
Kushagr Narayan <nkushagr@gmail.com>
2024-01-01 15:15:24 +0530

    header_started

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@
 import './App.css';
 import Register from './pages/Register';
 import Login from './pages/LoginPage'
+import Header from './components/Header'
 
 
 
 function App() {
   return (
     <div className="App bg-slate-50 text-lg">
-     <Register />
-    
+     <Header />
+     
     </div>
   );
 }
