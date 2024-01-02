8dc2a77be60d7ba071574d7c34fe2d159b480707
Kushagr Narayan <nkushagr@gmail.com>
2024-01-02 19:13:45 +0530

    header finished

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,17 +2,18 @@ import React from 'react'
 
 function Header() {
   return (
-    <div id="header" className='w-screen h-[15vh] bg-red-400 flex justify-between items-center px-20'>
+    <div id="header" className='w-screen h-[15vh] bg-red-400 flex justify-between items-center px-16'>
       <div id="header-name" className='text-white font-semibold text-lg '>
         Jobfinder
       </div>
-      <div id="profile">
-        <button className='bg-white'>
-          Login
-        </button>
-        <button className='bg-white'>
-          Register
-        </button>
+      <div id="profile" className='px-20'>
+      <button className=" text-white border-2 border-white text-center align-middle  px-5 rounded-lg focus:outline-none focus:shadow-outline text-lg w-auto h-[5vh] mx-1" type="button">
+       Login
+      </button>
+
+      <button className="bg-white text-red-400  text-center align-middle px-5 rounded-lg focus:outline-none focus:shadow-outline text-lg w-auto h-[5vh] mx-1 " type="button">
+       Register
+      </button>
       </div>
    
     </div>
