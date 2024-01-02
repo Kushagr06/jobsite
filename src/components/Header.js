e902d4dd27cf0054c89a2d0fd14cf778523a449c
Kushagr Narayan <nkushagr@gmail.com>
2024-01-02 14:14:06 +0530

    header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,8 +2,19 @@ import React from 'react'
 
 function Header() {
   return (
-    <div>
-      hello
+    <div id="header" className='w-screen h-[15vh] bg-red-400 flex justify-between items-center px-20'>
+      <div id="header-name" className='text-white font-semibold text-lg '>
+        Jobfinder
+      </div>
+      <div id="profile">
+        <button className='bg-white'>
+          Login
+        </button>
+        <button className='bg-white'>
+          Register
+        </button>
+      </div>
+   
     </div>
   )
 }
