7e0f5bd6cdc8a50e86ffda8e724ccf6696428b6c
Kushagr Narayan <nkushagr@gmail.com>
2024-01-04 16:49:00 +0530

    SearchBar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -9,16 +9,15 @@ function SearchBar() {
       <div className='flex justify-between'>
 
       <div className="my-4 flex align-middle m-1">
-        <select name="JobType" id="jobType" className=" border-2 rounded w-full py-2 px-3  leading-tight focus:outline-none focus:shadow-outline">
+        <select name="JobType" id="jobType" className=" border-2 rounded w-full py-2 px-3  leading-tight focus:outline-none focus:shadow-outline text-sm">
         <option value="">Skills:</option>
-        <option value="P+I">P+I</option>
-        <option value="I">I</option>
-        <option value="P">P</option>
+        <option value="html">html</option>
+        <option value="javascript">js</option>
+        <option value="React">React</option>
         </select>
 
         <div className=' px-2 py-3 bg-red-100 mx-2 text-center w-auto'>
-          html
-          
+          html  
         </div>
         <div className=' px-2 py-3 bg-red-100 mx-2 text-center w-auto'>
           javascript
@@ -26,11 +25,19 @@ function SearchBar() {
         <div className=' px-2 py-3 bg-red-100 mx-2 text-center w-auto'>
           Reactjs
         </div>
+      </div>
+
+      <div className="my-4 flex align-middle m-1">
+      <button className="bg-red-500 hover:bg-red-700 text-white font-semibold py-3 px-4 rounded focus:outline-none focus:shadow-outline text-sm" type="button">
+       + Add Job
+      </button>
+      </div>
 
       </div>
-      </div>
+      <div className=' px-3 text-red-500'><button className=' py-1 px-2 border-red-500 border-2 rounded-sm'>clear</button></div>
+      
     </div>
-    
+
     </div>
     </div>
   )
