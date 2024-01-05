653608a86608b89491c13b96a93ab49773580670
Kushagr Narayan <nkushagr@gmail.com>
2024-01-05 23:42:48 +0530

    context_pt_4

--- src/context/ProductContext.js
+++ src/context/ProductContext.js
@@ -1,19 +1,24 @@
-import { createContext, useContext, useReducer } from "react";
+import { createContext, useContext, useEffect, useReducer } from "react";
 import {data} from '../Data'
 import reducer from "./ProductReducer";
 
 const AppContext=createContext()
 
+const initialState={
+  products:[]
+};
 
 const AppProvider=({children})=>{
 
-  const initialState={
-    products:[]
-  };
-  const [state,dispatch]=useReducer(reducer, initialState)
-  const products=data;
-  dispatch({type:'API_DATA', payload:products})
 
+  const [state,dispatch]=useReducer(reducer, initialState);
+
+  const getProduct=()=>{dispatch({type:"API_DATA", payload:data})};
+
+  useEffect(()=>{
+    getProduct()
+  },[]);
+  
   return <AppContext.Provider value={{...state}}>
     {children}
   </AppContext.Provider>
