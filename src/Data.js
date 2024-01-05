e3097695013545d412772cfbac48dea2af9d2d6d
Kushagr Narayan <nkushagr@gmail.com>
2024-01-05 19:35:54 +0530

    context_pt_2

--- src/Data.js
+++ src/Data.js
@@ -1,10 +1,52 @@
 const data=
 [
 {
-  "id":1
+  "id":1,
+  "position":"FrontEnd Developer",
+  "no":10,
+  "stipend":50000,
+  "mode":"Office",
+  "time":"Full Time",
+  "location":"India",
+  "skills":[
+    "javascript",
+    "html",
+    "css"
+  ],
+  "image":"/"
+
 },
 {
-  "id":2
+  "id":2,
+  "position":"BackEnd Developer",
+  "no":10,
+  "stipend":50000,
+  "mode":"Office",
+  "time":"Full Time",
+  "location":"India",
+  "skills":[
+    "javascript",
+    "html",
+    "css"
+  ],
+  "image":"/"
+
+},
+{
+  "id":3,
+  "position":"BackEnd Developer",
+  "no":1,
+  "stipend":20000,
+  "mode":"Office",
+  "time":"Part Time",
+  "location":"India",
+  "skills":[
+    "javascript",
+    "html",
+    "css"
+  ],
+  "image":"/"
+
 }
 ]
 
